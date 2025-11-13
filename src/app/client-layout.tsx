5dd60cba857046a48715751ba4eee29c211ff12d
klinikOSapp <vicente@klinikos.ai>
2025-11-13 19:13:55 +0100

    login/register fornt done

--- src/app/client-layout.tsx
+++ src/app/client-layout.tsx
@@ -1,14 +1,11 @@
 'use client'
 
 import Layout from '@/components/layout/Layout'
-import { useAdaptiveDPI } from '@/hooks/useAdaptiveDPI'
 
 interface ClientLayoutProps {
   children: React.ReactNode
 }
 
 export default function ClientLayout({ children }: ClientLayoutProps) {
-  useAdaptiveDPI({ designWidth: 1920, baseRem: 10 })
-
   return <Layout>{children}</Layout>
 }
